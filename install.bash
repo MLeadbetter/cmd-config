f00bf8acc0dcf2665d0d6ab2bca3ce004d435049
Ubuntu <ubuntu@ip-172-31-8-139.us-west-2.compute.internal>
2022-03-26 09:05:56 +0000

    Added tablet public key

--- install.bash
+++ install.bash
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 sudo apt-get -y install python3-pip fish
-mkdir -p ~/.config/fish
-echo "set -Ux EDITOR vim" >> ~/.config/fish/config.fish
-sudo chsh -s $(which fish) $(whoami)
 sudo pip install flake8 green
+sudo chsh -s $(which fish) $(whoami)
 
 wget https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 mkdir -p ~/.vim/autoload
@@ -13,9 +11,6 @@ mv pathogen.vim ~/.vim/autoload
 ln -s .vimrc ~/.vimrc 
 ln -s .tmux.conf ~/.tmux.conf
 ln -s .tmux.conf.local ~/.tmux.conf.local
-ln -s .i3 ~/.i3
-ln -s .Xresources ~/.Xresources
-
 mkdir -p ~/.vim/bundle
 cd ~/.vim/bundle
 git clone https://github.com/vim-syntastic/syntastic.git
@@ -29,6 +24,7 @@ if [[ -z $is_ubuntu ]]; then
     git clone https://github.com/Valloric/YouCompleteMe
     cd YouCompleteMe
     freemem=$(free -bt | grep Mem | sed "s/^\s*Mem:\s*//" | cut -d' ' -f1)
+    echo $freemem
     git submodule update --init --recursive
     if [[ $freemem > 4*1024*1024*1024 ]]; then
         ./install.py --clang-completer
@@ -36,3 +32,5 @@ if [[ -z $is_ubuntu ]]; then
         ./install.py
     fi
 fi
+
+cat authorized_keys >> $(HOME)/.ssh/authorized_keys
